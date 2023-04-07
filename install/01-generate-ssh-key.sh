e75dbf5c9328c74235159f47397eab40cb1a2be7
bart geluykens <bart.geluykens@gmail.com>
2023-04-07 20:29:20 +0200

    git command

---
+++ install/01-generate-ssh-key.sh
@@ -0,0 +1,1 @@
+ssh-keygen -t ed25519 -b 16384 -C "bart.geluykens@gmail.com"
---
+++ install/02-install-git.sh
@@ -0,0 +1,3 @@
+sudo apt-get install git
+git config --global user.email "bart.geluykens@gmail.com"
+git config --global user.name "bart geluykens"
